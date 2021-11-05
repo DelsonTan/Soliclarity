bb42d03432e732fda16f4a69b8275dffbabdc23d
Delson <delsonwtan@gmail.com>
2021-11-04 20:57:14 -0700

    Add firebaseAPI_template.js (rename to firebaseAPI.js with config keys to use).

---
+++ scripts/firebaseAPI_template.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
