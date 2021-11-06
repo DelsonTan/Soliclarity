27d44a4127bf7032a4609a6b45b831dd9d2782e1
Colleen Vu <colleen_vu97@hotmail.com>
2021-11-05 17:22:47 -0700

    Modal added for index.html

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
