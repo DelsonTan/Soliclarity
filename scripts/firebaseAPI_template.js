b8d5fae296f64fddec8e6581c184c2ed7c4d9e97
Joshua Chen <joshuachenyyc@gmail.com>
2021-11-18 11:38:47 -0800

    fixed api

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA7MXZoaFeMaE2YJcGSql8dVZfAMSyD7vU",
-  authDomain: "soliclarity-dbb49.firebaseapp.com",
-  projectId: "soliclarity-dbb49",
-  storageBucket: "soliclarity-dbb49.appspot.com",
-  messagingSenderId: "468461573919",
-  appId: "1:468461573919:web:d5b87a2513a2b494b9c22a"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
