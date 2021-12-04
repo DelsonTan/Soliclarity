761cf1d5b84717d6d66f457f607d362b2ae0206d
Colleen Vu <colleen_vu97@hotmail.com>
2021-12-03 18:10:48 -0800

    remove unecessary css

--- scripts/index.js
+++ scripts/index.js
@@ -187,6 +187,5 @@ createEventModal.addEventListener("show.bs.modal", () => getCourseData());
 
 function editEvent() {
   //Enable the form fields
-  //console.log("edit is clicked");
   document.getElementById("personalInfoFieldsEdit").disabled = false; //make all of the buttons and fields activated (not disabled)
 }
